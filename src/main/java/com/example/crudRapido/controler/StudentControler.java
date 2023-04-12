7e4f0353f6530710257b2fd9915f505a4ded06b8
Dishyma <marojas4126@gmail.com>
2023-04-11 19:53:43 -0500

    add Student controler

--- src/main/java/com/example/crudRapido/controler/StudentControler.java
+++ src/main/java/com/example/crudRapido/controler/StudentControler.java
@@ -1,5 +1,42 @@
 package com.example.crudRapido.controler;
 
+import com.example.crudRapido.entity.Student;
+import com.example.crudRapido.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
 public class StudentControler {
-    
+    @Autowired
+    private final StudentService studentService;
+
+    public StudentControler(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> listStudent(){
+        return studentService.listStudent();
+    }
+
+    @GetMapping("/{studentId}")
+    public Optional<Student> getStudent(@PathVariable("studentId") Long studentId){
+        return studentService.getStudent(studentId);
+    }
+
+    @PostMapping
+    public void saveOrUpdate(@RequestBody Student student){
+        studentService.saveOrUpdate(student);
+    }
+
+    @DeleteMapping("/{studentId}")
+    public void delete(@PathVariable("studentId") Long studentId){
+        studentService.delete(studentId);
+    }
+
+
 }
